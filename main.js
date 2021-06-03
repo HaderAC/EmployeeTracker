fb4ce327b5052687c45b3efad097b8b5a9a00c89
HaderCore <Hadercortes@gmail.com>
2021-06-02 22:54:38 -0400

    created departements

--- main.js
+++ main.js
@@ -71,17 +71,69 @@ const employeeQuestions = () => {
         {
             name: "choices",
             type: "list",
-            choices: ["View all employees", "Add Employee", "Update Employee"]
+            choices: ["View all Employees", "Add Employee", "Update Employee"]
         }
     ])
 };
 
-const getQueryType = async () => {
-    console.log("leaf me alone")
-}
+const getQueryType = async (choice) => {
+    switch(choice) {
+        case "View Departments":
+            await getAllDepartments();
+            break;
+        case "Add Department":
+            await addDepartment();
+            break;
+        default:
+            init();
+            break;
+    }
+};
 
 const quitApp = () => {
     process.exit();
+};
+
+// SQL queries
+
+// SQL queries for deprtments
+const getAllDepartments = () => {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT * FROM departments;`
+
+        connectionA.query(query, (err, res) => {
+            if (err) reject(err);
+            else {
+                console.log("All Departments: \n\n");
+                console.table(res);
+                resolve(res);
+            }
+        });
+    });
+}
+
+const addDepartment = async () => {
+    return new Promise(async (resolve, reject) => {
+        const department = await inquirer.prompt([
+            {
+                name: "dept",
+                message: "Enter deptartment name"
+            }
+        ]);
+
+        const query = `
+        INSERT INTO departments (name)
+        VALUES (?);
+        `
+
+        connectionA.query(query, [department.dept], (err, res) => {
+            if (err) reject(err);
+            else {
+                console.log("\n Successfully created department");
+                resolve(res);
+            }
+        });
+    });
 }
 
 module.exports = connect;
