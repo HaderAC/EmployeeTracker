8763f50e7836593782df0723c0ded750740196c0
HaderCore <Hadercortes@gmail.com>
2021-06-02 22:34:49 -0400

    Prompts for users

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ const connect = (connection) => {
 
 const init = async() => {
     const userChoice = await promptResponse();
+
+    const functionPicked = await switchChoices(userChoice.choices);
+
 }
 
 const promptResponse = ()=>{
@@ -18,7 +21,56 @@ const promptResponse = ()=>{
         type:"list",
         choices:["Department", "Roles", "Employees", "Quit"],
     }])
+};
 
-}
+const switchChoices = async (choices) => {
+    let answer;
+    switch(choices) {
+        case "Department":
+            answer = await departmentQuestions();
+            break;
+        case "Roles":
+            answer = await roleQuestions();
+            break;
+        case "Employees":
+            answer = await employeeQuestions();
+            break;
+        default:
+            quitApp();
+            break;
+    }
+
+    return answer;
+};
+
+const departmentQuestions = () => {
+    return inquirer.prompt([
+        {
+            name: "choices",
+            type: "list",
+            choices: ["View Departments", "Add Department"]
+        }
+    ])
+};
+
+const roleQuestions = () => {
+    return inquirer.prompt([
+        {
+            name: "choices",
+            type: "list",
+            choices: ["View Roles", "Add Role"]
+        }
+    ])
+};
+
+const employeeQuestions = () => {
+    return inquirer.prompt([
+        {
+            name: "choices",
+            type: "list",
+            choices: ["View all employees", "Add Employee", "Update Employee"]
+        }
+    ])
+};
 
 module.exports = connect;
